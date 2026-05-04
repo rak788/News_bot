06e8850b0cfedba85cff6078e5a538f0490d74a9
rak788 <rak8007@proton.me>
2026-05-04 03:44:12 +0300

    Update news_bot.py

--- news_bot.py
+++ news_bot.py
@@ -11,7 +11,7 @@ CHAT_ID = os.environ.get("CHAT_ID", "")
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "")
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 RSS_FEEDS = {
     "Finance": [
